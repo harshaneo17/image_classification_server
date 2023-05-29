bd6d3727ef64123f335c4dc2bed083ab3b6da43c
harshaneo17 <harsha.god86@gmail.com>
2023-05-29 18:59:02 +0100

    changes to the readme and log statements

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from PIL import Image
-from model_predict import read_image
-from model_predict import transform
+from model_predict import read_image, transform, resultsjson
 from fastapi import FastAPI, File, UploadFile
 from io import BytesIO
 
-
-app = FastAPI()
+app = FastAPI() #load fastapi
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.post("/files/")
 async def create_file(file: bytes = File(...)):
-  
     return {"file_size": len(file)}
     
-
-
-@app.get("/uploadfile/")
+@app.post("/uploadfile/")
 async def main(file: bytes = File(...)):
-
     # read image
     imagem = read_image(file)
-    # transform and prediction 
-    prediction = transform(imagem)
-
+    # transform and get result 
+    result = transform(imagem)
+    # get prediction
+    prediction = resultsjson(result)
     return prediction
--- model_predict.py
+++ model_predict.py
@@ -7,37 +7,44 @@ from tensorflow.keras.applications.resnet50 import decode_predictions as decode_
 import numpy as np
 from PIL import Image
 from io import BytesIO
+import logging as log
 
-model = ResNet50(weights='imagenet')
-model2 = VGG16(weights='imagenet')
+log.basicConfig(level=log.INFO) #sets the basic logging level as info
+
+
+model = VGG16(weights='imagenet')   #load pretrained vgg16 model with imagenet weights
+model2 = ResNet50(weights='imagenet') #load pretrained resnet50 model with imagenet weights
+
 
 def read_image(file) -> Image.Image:
+    '''This function takes file and converts it into PIL image format'''
     pil_image = Image.open(BytesIO(file))
-    #insert log statement
+    log.info('_____LOADING IMAGE______')
     return pil_image
 
-def transform(file: Image.Image):
-
-    #img_path = 'image3.jpeg'
-    #img = image.load_img(file, target_size=(224, 224))
-    img = np.asarray(file.resize((224, 224)))[..., :3]
+def transform(file) -> Image.Image:
+    '''This function takes the converted image and makes prediction on that after preprocessing it. 
+    Returns the decoded prediction of ensemble algorithms '''
+    img = np.asarray(file.resize((224, 224)))[..., :3] #loads the image in standard 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    y = x.copy()
+    x , y = preprocess_input(x),preprocess_input2(y) #uses different
     preds = model.predict(x)
-# decode the results into a list of tuples (class, description, probability)
-# (one such list for each sample in the batch)
-    #result = {}
-    print('Predicted:', decode_predictions(preds, top=3)[0])
-    #result = decode_predictions(preds, top=3)[0]
-    result = decode_predictions(model.predict(x), 3)[0]
+    preds2 = model2.predict(y)
+    averaged_predictions = np.mean([preds, preds2], axis=0) #ensemble methods with average of two predictions
+
+    log.info(f'Predicted:{decode_predictions(averaged_predictions, top=1)[0]}')
+    result = decode_predictions(averaged_predictions, 1)[0]
+    return result
     
+def resultsjson(listobject) -> list:
+    '''This function takes listobject from the above function and makes it a response object.
+    Returns response object'''
     response = []
-    for i, res in enumerate(result):
+    for i, res in enumerate(listobject):
         resp = {}
         resp["class"] = res[1]
         resp["confidence"] = f"{res[2]*100:0.2f} %"
-
         response.append(resp)
-
     return response
